523d48dcf9d9c234d155f6ac59b993e851198f7f
Sergey <keksman89@gmail.com>
2018-09-19 11:00:00 +0300

    initial commit

---
+++ android/src/ru/gb_shisheya/stargame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.gb_shisheya.stargame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.gb_shisheya.stargame.App;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new App(), config);
+	}
+}
---
+++ desktop/src/ru/gb_shisheya/stargame/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package ru.gb_shisheya.stargame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.gb_shisheya.stargame.App;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new App(), config);
+		config.width = 512;
+		config.height = 512;
+	}
+}
